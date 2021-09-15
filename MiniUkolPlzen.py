90050d537961fb6f8e4d91264151264f71df70d2
Jakub <43874680+xhaj06@users.noreply.github.com>
2021-09-15 20:21:08 +0200

    Update MiniUkolPlzen.py
    Dle zadání hledá články na české wikipedii.

--- MiniUkolPlzen.py
+++ MiniUkolPlzen.py
@@ -1,43 +1,30 @@
-import sys
-import glob
-import os
+import wikipedia
+wikipedia.set_lang("cs")
+encoding = 'utf-8'
 
-nalezeno = 0
+def Zadej():
 
-#-------Nazev Input-------
-if __name__== "__main__":
-    try:
-        name = sys.argv[1]
-    except:
-        name = input("Zadejte název článku\n")
+	if __name__== "__main__":
+		try:
+			name = sys.argv[1]
+		except:
+			name = input("Zadejte název článku\n")
+	print ("---------")
+	return name
 
 
-print ("---------")
+def hledani(slovo):
+	print(f'Článek s názvem "{slovo}" nebyl nalezen. Zadaný text se vyskytuje v článcích s tímto názvem:')
+	print(wikipedia.search(name))
 
-#-------Hledani Clanku--------
-
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-for file in glob.glob("*.txt"):
-	if file[:-4]==name:
-		f = open(file ,'r',encoding="utf8")
-		file_contents = f.read()
-		print(file_contents)
-		
-		nalezeno=1
-
-#-------Hledani Slova-------
-
-if nalezeno == 0:
-	for file in glob.glob("*.txt"):
-		f = open(file ,'r',encoding="utf8")
-		file_contents = f.read()
-		if name in file_contents:
-			if nalezeno==0:
-				print(f'Článek s názvem "{name}" nebyl nalezen. Zadaný text se vyskytuje v článcích s tímto názvem:')
-				nalezeno=1
-			print(file[:-4])
-			
-
-if nalezeno == 0:
-	print("Bohuzel jsme nenašli žádny soubor ani text odpovidající zadaným parametrům")
-	
+name = Zadej ()
+try:
+	nalez = wikipedia.page(name)
+	print (nalez.title)
+	if str.lower(nalez.title) == str.lower(name):
+		shrnuti= str(wikipedia.summary(name))
+		print (shrnuti)
+	else: 
+		hledani(name)
+except wikipedia.exceptions.PageError: 
+	hledani(name)
